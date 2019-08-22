807ba0fa976d1feee62bc628bf0c344ae93d90b6
Surbhit Wagle <swagle.iet21@gmail.com>
2019-08-23 02:19:57 +0530

    Credits scene added along with keystore for google play publishing

---
+++ Assets/Scripts/MainApplication/OpenUrl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenUrl : MonoBehaviour
+{
+    public void OpenURL(string url)
+      {
+          Application.OpenURL(url);
+          Debug.Log("is this working?");
+      }
+}
